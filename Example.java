75ba9dd0cf8be6a1e7c6c10f3e5593762db95362
Pasandul Kasthuri <nethmika86@gmail.com>
2026-04-25 20:39:03 +0530

    update

--- Example.java
+++ Example.java
@@ -1,15 +1,11 @@
 import java.util.*;
 class Example{
-	public static void main (String args[]){
-		Scanner input = new Scanner(System.in);
-		System.out.println("Enter Marks : ");
-		double marks=input.nextDouble();
-		
-		//System.out.println("A");
-		if (marks>50){
-		System.out.println("You are Pass ");
-		}else{
-		System.out.println("You are fail ");
-		}
+	public static void main(String args[]){
+				Scanner input = new Scanner(System.in);
+					//int x =-2;
+					int y=Math.abs(-2);
+					System.out.println(y);
+	
+	}
 }
-}		
+	
--- demo.java
+++ demo.java
@@ -1,24 +1,59 @@
 import java.util.*;
 class demo{
-   public static void main(String args[]){
-		Scanner input = new Scanner(System.in);
-		System.out.println("Enter 1 for DecimalToBinary");
-		System.out.println("Enter 2 for DecimalToOctal");
-		System.out.println("Enter 3 for DecimalToHexa");
-		System.out.println("Enter 99 for Exit");
-		//int x = input.nextInt();
-		System.out.println();
-		System.out.print("Select the converter: ");
-		int x = input.nextInt();
-		System.out.println();
-		if(x==1){
-			System.out.println("DecimalToBinary Converter"); // 1
-		}else if(x==2){
-			System.out.println("DecimalToOctal Converter"); // 2
-		}else if(x==3){
-			System.out.println("DecimalToHexa Converter"); // 3
-		}else{
-		System.out.println("Invalid Input...");
-		}
+	public static int withHoldingtax(){
+		Scanner input=new Scanner(System.in);
+			System.out.println("");
+			System.out.println("\t\t\tWithhodling tax\t\t\t");
+			System.out.println("");
+			//int x2=input.nextInt();
+			System.out.println("[1] calculate rent tax");
+			System.out.println("[2] calculate bank interest tax");
+			System.out.println("[3] calculate dividend tax");
+			System.out.print("Enter what you wanna do : ");
+			int x2=input.nextInt();
+			int x5= x2;
+			return x5;
+			
 	}
-}   
+	public static void payabletax(){
+		Scanner input=new Scanner(System.in);
+			System.out.println("");
+			System.out.println("\t\t\tpayable tax\t\t\t");
+			System.out.println("");
+			System.out.print("Enter what you wanna do : ");
+			
+	}
+	
+	public static void main(String args[]){
+		Scanner input=new Scanner(System.in);
+		System.out.println("Hello User!\n");
+		System.out.println("///\t\t\tTax Calculator\t\t\t\\\\\\");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("[1] Calculate withholfing tax");
+		System.out.println("[2] calculate payable tax");
+		System.out.println("[3] calculate income tax");
+		System.out.println("[4] calculate socila security...");
+		System.out.println("[5] calculate leasing payment");
+		System.out.println("");
+		System.out.print("Enter what you wanna do : ");
+		int x1=input.nextInt();
+		switch(x1){
+			case 1 : {
+						int x6=withHoldingtax();
+						System.out.println("return value "+x6);
+						break;
+					}
+			case 2 : {
+						payabletax();
+						break;
+					}
+			default :{
+						System.out.println("\n\nInvalid input! enter a valid number...\n");
+						main(new String[]{""});
+					}
+					
+
+	}
+}
+}
