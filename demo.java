bfa4c828d61ba04da890bc27ff48bb0a20d092f3
Pasandul Kasthuri <nethmika86@gmail.com>
2026-04-30 00:06:47 +0530

    update

--- demo.java
+++ demo.java
@@ -49,7 +49,7 @@ class demo{
 						break;
 					}
 			default :{
-						System.out.println("\n\nInvalid input! enter valid number...\n");
+						System.out.println("\n\nInvalid input! enter  valid number...\n");
 						main(new String[]{""});
 					}
 					
