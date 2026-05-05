00e47c66cd67c7d7f5f30cb828b7a1cc1745e503
Pasandul Kasthuri <nethmika86@gmail.com>
2026-05-06 01:02:01 +0530

    update

--- demo.java
+++ demo.java
@@ -9,7 +9,7 @@ class demo{
 			System.out.println("[1] calculate rent tax ");
 			System.out.println("[2] calculate bank interest tax");
 			System.out.println("[3] calculate dividend tax");
-			System.out.print("Enter what you wanna do : ");
+			System.out.print("Enter w  hat you wanna do : ");
 			int x2=input.nextInt();
 			int x5= x2;
 			return x5;
