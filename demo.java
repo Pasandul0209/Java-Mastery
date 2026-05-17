54acc7ddfd761572381cff00413e23573d677b53
Pasandul Kasthuri <nethmika86@gmail.com>
2026-05-17 19:18:12 +0530

    update

--- demo.java
+++ demo.java
@@ -8,6 +8,7 @@ class demo{
 			int x2 = input.nextInt();
 			if(c_id.charAt(0) == -1){
 				System.exit(0);
+				
 				if(c_id.charAt(3)!=0 & c_id.charAt(3)>0);
 				System.out.println("Enetr Customer ID (Enter -1 to stop) : " + c_id);
 				System.out.println("Enter Bill value : "+x2);
