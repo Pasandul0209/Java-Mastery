f0142655f4b3e35394e61ef6fdc137eafcae4ada
Pasandul Kasthuri <nethmika86@gmail.com>
2026-05-17 19:18:40 +0530

    update

--- demo.java
+++ demo.java
@@ -9,7 +9,6 @@ class demo{
 			if(c_id.charAt(0) == -1){
 				System.exit(0);
 
-				
 				if(c_id.charAt(3)!=0 & c_id.charAt(3)>0);
 				System.out.println("Enetr Customer ID (Enter -1 to stop) : " + c_id);
 				System.out.println("Enter Bill value : "+x2);
